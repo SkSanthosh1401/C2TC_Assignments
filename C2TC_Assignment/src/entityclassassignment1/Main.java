f78ae735e0a1042e986c8ba81bbff0a6095ded8b
Lenovo <Lenovo@DESKTOP-6UOCQ3I>
2025-08-01 13:51:17 +0530

    THIS IS MY ASSIGNMENTS

---
+++ C2TC_Assignment/src/entityclassassignment1/Main.java
@@ -0,0 +1,16 @@
+package entityclassassignment1;
+
+class Main {
+	
+	 public static void main(String[] args) 
+	 {
+	        Student s1 = new Student("Gokul", 20, "IT");
+	        Student s2 = new Student("Anu", 21, "CSE");
+	        Student s3 = new Student("Raj", 22, "ECE");
+
+	        System.out.println("Student Details:");
+	        s1.display();
+	        s2.display();
+	        s3.display();
+	  }
+}
